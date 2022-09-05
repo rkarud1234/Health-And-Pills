0d9646b0873b413cf034169e0e2e8eccd1b04851
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-09-05 21:33:03 +0900

    fix: userService 복구

--- BE/hp-api/src/main/java/com/ssafy/hp/user/service/UserServiceImpl.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/user/service/UserServiceImpl.java
@@ -1,23 +1,4 @@
 package com.ssafy.hp.user.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class UserServiceImpl extends DefaultOAuth2UserService {
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        // OAuth로 넘어온 사용자 profile scope가 전처리되는 메서드
-        return null;
-    }
-
-    private OAuth2User proccessOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
-        // 기존 회원인지 신규 회원인지 판단
-        return null;
-    }
+public class UserServiceImpl {
 }
