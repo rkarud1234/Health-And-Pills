682507f8a4db3ab4db9ece45e8285c0a558afb20
mindot7 <codemj11@gmail.com>
2022-09-07 16:51:00 +0900

    feat: 운동 종류 탭별 페이지 추가

---
+++ FE/src/pages/health/HealthFindPart/HealthFindBody.js
@@ -0,0 +1,14 @@
+import HealthCard from "../../../components/cards/HealthCard";
+
+const HealthFindBody = () => {
+  return (
+    <>
+      맨몸 운동 페이지
+      <HealthCard width={"400px"}/>
+      <HealthCard width={"400px"}/>
+      <HealthCard width={"400px"}/>
+    </>
+  );
+};
+
+export default HealthFindBody;
