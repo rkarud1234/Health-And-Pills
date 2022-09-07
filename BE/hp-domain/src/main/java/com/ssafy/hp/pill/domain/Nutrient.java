52824c2826d24e5d53422eaab5c8e5784c16f7cb
kkk <rkarud1234@koreatech.ac.kr>
2022-09-07 14:13:54 +0900

    feat: Nutrient 엔티티 생성

---
+++ BE/hp-domain/src/main/java/com/ssafy/hp/pill/domain/Nutrient.java
@@ -0,0 +1,26 @@
+package com.ssafy.hp.pill.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Nutrient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "nutrient_id")
+    private int nutrientId;
+
+    private String nutrientName;
+
+    @OneToMany(mappedBy = "nutrient", cascade = CascadeType.ALL)
+    private List<PillNutrient> pillNutrients = new ArrayList<>();
+}
