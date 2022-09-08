90f60d86e714ee11e0eefe783559cff54bc8b0b7
Sang Jin <tkdwls8412@naver.com>
2022-09-08 15:17:06 +0900

    feat: 영양제 추천 리스트 컴포넌트 구현

--- FE/src/pages/Pills/RecomPills.js
+++ FE/src/pages/Pills/RecomPills.js
@@ -1,10 +1,8 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 // import { useSelector } from "react-redux";
 import PillCard from '../../components/cards/PillCard.js'
-import Lutein from '../../assets/lutein.jpg'
-import Omega3 from '../../assets/omega3.jpg'
-import Cmbzmulti from '../../assets/cmbzmulti.jpg'
+
 
 const TextDiv = styled.div`
 margin: 24px 0 0 16px;
@@ -21,42 +19,60 @@ margin: 0px 16px
 const FlexBox = styled.div`
 ::-webkit-scrollbar {
   display: none;
-} /* Chrome, Safari, Opera*/
-scrollbar-width: none; /* firefox */
+} /* Chrome, Safari, Opera 환경*/
+scrollbar-width: none; /* firefox 환경 */
 cursor: pointer;
 display: flex;
 overflow-x: scroll;
 padding: 16px 0px 24px;
 `
 
-const RecomPills = () => {
+const RecomPills = ({ pills, type }) => {
   // const user = useSelector((state) => state.user);
   const username = '춘식이'
-  const pills = [
-    { id: 1, text: '루테인', rating: 4.5, url: Lutein },
-    { id: 2, text: '오메가3', rating: 4.7, url: Omega3 },
-    { id: 3, text: '종합비타민', rating: 4.9, url: Cmbzmulti },
-    { id: 4, text: '루테인', rating: 4.5, url: Lutein },
-    { id: 5, text: '오메가3', rating: 4.7, url: Omega3 },
-    { id: 6, text: '종합비타민', rating: 4.9, url: Cmbzmulti },
-  ]
+
+  // 클릭 앤 드래그로 스크롤 이동시키기
+  const scrollRef = useRef(null);
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState();
+
+  const onDragStart = (e) => {
+    e.preventDefault();
+    setIsDrag(true);
+
+    setStartX(e.pageX + scrollRef.current.scrollLeft);
+  };
+
+  const onDragEnd = () => {
+    setIsDrag(false);
+  };
+
+  const onDragMove = (e) => {
+    if (isDrag) {
+      scrollRef.current.scrollLeft = startX - e.pageX;
+    }
+  };
+
+  let text = null
+
+  if (type === 'user') {
+    text = <Text>'{username}' 님을 위한 맞춤 영양제 추천</Text>
+  } else if (type === 'age') {
+    text = <Text>20대 여성이 많이 찾는 영양제 추천</Text>
+  }
+
   return (
     <>
       <TextDiv>
-        <Text>'{username}' 님을 위한 맞춤 영양제 추천</Text>
+        {text}
       </TextDiv>
-      <FlexBox>
-        {pills ? pills.map(pill => {
-          return (<CardDiv key={pill.id}>
-            <PillCard url={pill.url} text={pill.text} rating={pill.rating} />
-          </CardDiv>)
-        }) : <></>}
-      </FlexBox>
-
-      <TextDiv>
-        <Text>20대 여성이 많이 찾는 영양제 추천 </Text>
-      </TextDiv>
-      <FlexBox>
+      <FlexBox
+        ref={scrollRef}
+        onMouseDown={onDragStart}
+        onMouseMove={onDragMove}
+        onMouseUp={onDragEnd}
+        onMouseLeave={onDragEnd}
+      >
         {pills ? pills.map(pill => {
           return (<CardDiv key={pill.id}>
             <PillCard url={pill.url} text={pill.text} rating={pill.rating} />
