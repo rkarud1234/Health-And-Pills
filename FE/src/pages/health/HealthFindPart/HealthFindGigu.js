48639ee82e84cebc99bb87efdd69595d876343c1
김민정 <codemj11@gmail.com>
2022-09-08 11:53:06 +0900

    [20220908] FE: 운동 페이지 탭별 분류 및 카드 컴포넌트 적용

---
+++ FE/src/pages/health/HealthFindPart/HealthFindGigu.js
@@ -0,0 +1,15 @@
+import HealthCard from "../../../components/cards/HealthCard";
+
+const HealthFindGigu = () => {
+
+  return (
+    <>
+      기구운동 페이지
+      <HealthCard width={"400px"}/>
+      <HealthCard width={"400px"}/>
+      <HealthCard width={"400px"}/>
+    </>
+  );
+};
+
+export default HealthFindGigu;
---
+++ FE/src/pages/health/HealthRecommend.js
@@ -0,0 +1,56 @@
+import HealthCard from "../../components/cards/HealthCard";
+import styled from "styled-components";
+import Carousel from "../../components/carousel/Carousel";
+import imgUrl from "../../assets/togetherX.jpg";
+
+const RecommendWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const HealthRecommend = () => {
+
+  const images = [
+    { id: 1, url: imgUrl },
+    { id: 2, url: imgUrl },
+    { id: 3, url: imgUrl },
+    { id: 4, url: imgUrl },
+    { id: 5, url: imgUrl },
+    { id: 6, url: imgUrl },
+  ]
+
+  return (
+    <>
+      <div>
+        <Carousel images={images} />
+      </div>
+      <div>
+          추천 1
+      </div>
+      <RecommendWrapper>
+        <HealthCard/>
+        <HealthCard/>
+        <HealthCard/>
+      </RecommendWrapper>
+      <div>
+        추천 2
+      </div>
+      <RecommendWrapper>
+        <HealthCard/>
+        <HealthCard/>
+        <HealthCard/>
+      </RecommendWrapper>
+      <div>
+        추천 3
+      </div>
+      <RecommendWrapper>
+        <HealthCard/>
+        <HealthCard/>
+        <HealthCard/>
+      </RecommendWrapper>
+    </>
+  );
+};
+
+export default HealthRecommend;
