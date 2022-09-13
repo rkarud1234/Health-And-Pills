916a2d82b394cf1d68bc8f3bf2cd1ad777d95f79
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-09-13 18:02:52 +0900

    feat: 운동컨트롤러 구조 작업

---
+++ BE/hp-api/src/main/java/com/ssafy/hp/exercise/controller/ExerciseController.java
@@ -0,0 +1,10 @@
+package com.ssafy.hp.exercise.controller;
+
+import lombok.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/exercise")
+@RequiredArgsConstructor
+public class ExerciseController {
+}
---
+++ BE/hp-api/src/main/java/com/ssafy/hp/exercise/service/ExerciseServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ssafy.hp.exercise.service;
+
+public class ExerciseServiceImpl implements ExerciseService {
+}
