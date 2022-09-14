51c6ca7a78ac6873c24fc126b608e5b9b4668544
이민우 <minwoo9629@naver.com>
2022-09-14 10:24:41 +0900

    Feat : 마이페이지 관련 컴포넌트 추가

---
+++ FE/src/components/user/UserInfoListItem.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { useRef } from "react";
+import styled from "styled-components";
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 20px;
+  position: relative;
+`;
+
+const ItemImgTitleWrapper = styled.div`
+  display: flex;
+`;
+
+const ItemImgWrapper = styled.div`
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  border: 1px solid #ddd;
+  margin-right: 14px;
+  font-size: 20px;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+  @media screen and (max-width: 280px) {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+const ItemTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  line-height: 20px;
+`;
+
+const ItemTitle = styled.p`
+  font-size: 20px;
+  & + span {
+    font-size: 14px;
+  }
+  & + span i {
+    color: blue;
+    margin-right: 5px;
+  }
+  @media screen and (max-width: 280px) {
+    font-size: 14px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & button {
+    font-size: 20px;
+    background-color: transparent;
+  }
+`;
+const UserInfoListItem = ({ id, title, img, rating, deleteUserInfo, type }) => {
+  const itemRef = useRef();
+  return (
+    <ItemWrapper ref={itemRef}>
+      <ItemImgTitleWrapper>
+        <ItemImgWrapper>
+          {img !== undefined ? (
+            <img src={process.env.PUBLIC_URL + "/logo512.png"} />
+          ) : type === "pill" ? (
+            <i className="fa-regular fa-pills"></i>
+          ) : (
+            <i className="fa-sharp fa-solid fa-person-walking"></i>
+          )}
+        </ItemImgWrapper>
+        <ItemTitleWrapper>
+          <ItemTitle>{title}</ItemTitle>
+          {rating !== undefined ? (
+            <span>
+              <i className="fa-solid fa-star"></i>
+              {rating}
+            </span>
+          ) : (
+            <></>
+          )}
+        </ItemTitleWrapper>
+      </ItemImgTitleWrapper>
+      <ButtonWrapper>
+        <button
+          onClick={() => {
+            console.log(itemRef);
+            console.log(itemRef.current.style);
+            deleteUserInfo(type, id);
+          }}
+        >
+          <i className="fa-regular fa-trash"></i>
+        </button>
+      </ButtonWrapper>
+    </ItemWrapper>
+  );
+};
+
+export default React.memo(UserInfoListItem);
