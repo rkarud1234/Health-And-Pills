23d69f286288c87a1441e91ebcfa8c08c131e33f
mindot7 <codemj11@gmail.com>
2022-09-15 22:38:36 +0900

    chore: 북마크 색 수정

--- FE/src/components/buttons/BookMark.js
+++ FE/src/components/buttons/BookMark.js
@@ -6,10 +6,10 @@ const MarkStyled = styled.button`
   padding: 4px 6px;
   background-color: transparent;
   color: #39f2ac;
-  position: absolute;
   background: linear-gradient(#537CFE, #6A53FE);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  font-size: large;
 `
 
 const BookMark = () => {
