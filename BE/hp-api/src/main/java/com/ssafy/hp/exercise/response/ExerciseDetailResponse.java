b8e4e33d549852a061c741c5c0026bd5a71a24c4
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-09-15 10:10:52 +0900

    feat: response 타입 생성

--- BE/hp-api/src/main/java/com/ssafy/hp/exercise/response/ExerciseDetailResponse.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/exercise/response/ExerciseDetailResponse.java
@@ -11,10 +11,16 @@ import lombok.*;
 @AllArgsConstructor
 public class ExerciseDetailResponse {
     String exerciseName; // 운동이름
+
     Aerobic aerobic; // 유무산소
+
     Integer exercisePart; // 운동부위
+
     Integer exerciseCategory; // 운동 분류
+
     YN bookmark; // 북마크여부
+
     YN doing; // 운동중여부
+
     YN like; // 좋아요여부
 }
--- BE/hp-api/src/main/java/com/ssafy/hp/exercise/response/ExerciseListResponse.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/exercise/response/ExerciseListResponse.java
@@ -14,9 +14,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ExerciseListResponse {
     String exerciseName; // 운동이름
+
     Aerobic aerobic; // 유무산소
+
     Integer exercisePart; // 운동부위
+
     Integer exerciseCategory; // 운동 분류
+
     YN bookmark; // 북마크여부
+
     YN doing; // 운동중여부
 }
