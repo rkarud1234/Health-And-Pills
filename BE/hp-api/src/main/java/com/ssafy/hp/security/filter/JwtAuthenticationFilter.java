54115ba0a53314b24f36535a5dbca5b9bfe0a510
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-09-16 11:21:02 +0900

    fix: oauth로그인을 위한 설정 추가

--- BE/hp-api/src/main/java/com/ssafy/hp/security/filter/JwtAuthenticationFilter.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/security/filter/JwtAuthenticationFilter.java
@@ -3,7 +3,7 @@ package com.ssafy.hp.security.filter;
 import com.ssafy.hp.security.service.UserDetailService;
 import com.ssafy.hp.security.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,7 +12,8 @@ import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
 import org.springframework.stereotype.Component;
-import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @Component
@@ -22,12 +23,34 @@ public class JwtAuthenticationFilter extends GenericFilter {
     private final JwtUtil jwtUtil;
     private final UserDetailService userDetailService;
 
+//    @Override
+//    public void doFilter(ServletRequest request,
+//                         ServletResponse response,
+//                         FilterChain chain) throws IOException, ServletException {
+//
+//        String jwtToken = extractToken((HttpServletRequest) request);
+//
+//        if (StringUtils.hasText(jwtToken) && jwtUtil.isValidToken(jwtToken)) {
+//            UserDetails userDetails = userDetailService.loadUserByUsername(jwtUtil.getSubject(jwtToken));
+//
+//            Authentication authentication =
+//                    new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//        }
+//        chain.doFilter(request, response);
+//    }
     @Override
     public void doFilter(ServletRequest request,
                          ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
 
-        String jwtToken = extractToken((HttpServletRequest) request);
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");     //허용할 request http METHOD : POST, GET, DELETE, PUT
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
+
+        String jwtToken = extractToken((HttpServletRequest) req);
 
         if (StringUtils.hasText(jwtToken) && jwtUtil.isValidToken(jwtToken)) {
             UserDetails userDetails = userDetailService.loadUserByUsername(jwtUtil.getSubject(jwtToken));
@@ -36,7 +59,12 @@ public class JwtAuthenticationFilter extends GenericFilter {
                     new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        chain.doFilter(request, response);
+
+        if(req.getMethod().equals(HttpMethod.OPTIONS.name())){
+            res.setStatus(HttpStatus.OK.value());
+        }else{
+            chain.doFilter(req, res);
+        }
     }
 
     private String extractToken(HttpServletRequest request) {
