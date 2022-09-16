14383d2c07ac7c97ce8ddf3cd3bfa55763849e91
이민우 <minwoo9629@naver.com>
2022-09-16 16:35:27 +0900

    Feat : 소셜로그인을 위한 redirect 페이지

---
+++ FE/src/pages/user/SocialLogin.js
@@ -0,0 +1,20 @@
+import { useDispatch } from "react-redux";
+import { useNavigate, useSearchParams } from "react-router-dom";
+// import { SuccessAlert } from "../../utils/sweetAlert";
+// import { getUserProfile } from "../../store/actions/user";
+
+const SocialLogin = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const accessToken = searchParams.get("accesstoken");
+  const refreshToken = searchParams.get("refreshtoken");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  sessionStorage.setItem("ACCESS_TOKEN", accessToken);
+  sessionStorage.setItem("REFRESH_TOKEN", refreshToken);
+  console.log("냥냥");
+  // dispatch(getUserProfile());
+  // SuccessAlert("로그인되었습니다", navigate);
+  return <></>;
+};
+
+export default SocialLogin;
