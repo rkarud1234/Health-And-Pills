297d15fa8fa987f61cf485c67e90d18a5d92bd38
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-09-19 02:12:05 +0900

    feat: 운동종류별 조회 api를 위한 repository 메서드 추가

---
+++ BE/hp-domain/src/main/java/com/ssafy/hp/exercise/ExercisePartCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.hp.exercise;
+
+import com.ssafy.hp.exercise.domain.*;
+import org.springframework.data.jpa.repository.*;
+
+public interface ExercisePartCategoryRepository extends JpaRepository<ExercisePartCategory, Integer> {
+}
+
