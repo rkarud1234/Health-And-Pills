891e00873189d06df4402688f8d9844b4ac33bcf
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-09-19 02:10:14 +0900

    feat: 운동종류별 운동 반환 기능 구현

--- BE/hp-api/src/main/java/com/ssafy/hp/exercise/controller/ExerciseController.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/exercise/controller/ExerciseController.java
@@ -1,15 +1,29 @@
 package com.ssafy.hp.exercise.controller;
 
+import com.ssafy.hp.config.*;
 import com.ssafy.hp.exercise.response.*;
+import com.ssafy.hp.exercise.service.*;
+import com.ssafy.hp.user.domain.*;
 import lombok.*;
 import org.springframework.data.domain.*;
+import org.springframework.data.web.*;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/exercise")
 @RequiredArgsConstructor
-public class ExerciseController {// 운동 종류별 조회
-//    Page<ExerciseListResponse> findByExerciseCategory(@RequestParam Integer category, Pageable pageable);
+public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+
+    // 운동 종류별 조회
+    @GetMapping("/category")
+    ResponseEntity<Page<ExerciseListResponse>> findByExerciseCategory(@LoginUser User user, @RequestParam Integer category, @PageableDefault(size = 10) Pageable pageable) {
+        System.out.println("user.getUserId() = " + user.getUserId());
+        Page<ExerciseListResponse> body = exerciseService.findByExerciseCategory(null, category, pageable);
+        return ResponseEntity.ok().body(body);
+    }
 //
 //    // 운동 부위별 조회
 //    Page<ExerciseListResponse> findByExercisePart(@RequestParam Integer part, Pageable pageable);
