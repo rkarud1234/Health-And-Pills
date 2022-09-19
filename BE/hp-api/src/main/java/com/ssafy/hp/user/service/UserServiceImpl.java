24bf1306b38506f9e31a0809fabba5040f8568f2
gksekqls9808 <gksekqls9808@naver.com>
2022-09-19 17:28:06 +0900

    chore: userservice 회원 운동&상품 조회 기능 구현

--- BE/hp-api/src/main/java/com/ssafy/hp/user/service/UserServiceImpl.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/user/service/UserServiceImpl.java
@@ -3,18 +3,17 @@ package com.ssafy.hp.user.service;
 import com.ssafy.hp.NotFoundException;
 import com.ssafy.hp.auth.AuthRepository;
 import com.ssafy.hp.auth.domain.Auth;
+import com.ssafy.hp.exercise.ExerciseRepository;
+import com.ssafy.hp.exercise.domain.Exercise;
+import com.ssafy.hp.pill.PillRepository;
+import com.ssafy.hp.pill.domain.Pill;
 import com.ssafy.hp.pill.domain.PillReview;
-import com.ssafy.hp.user.ExercisePurposeRepository;
-import com.ssafy.hp.user.UserProfileRepository;
-import com.ssafy.hp.user.UserRepository;
+import com.ssafy.hp.user.*;
 import com.ssafy.hp.user.domain.*;
 import com.ssafy.hp.user.query.UserQueryRepository;
 import com.ssafy.hp.user.request.UpdateUserExerciseRequest;
 import com.ssafy.hp.user.request.UpdateUserInbodyRequest;
-import com.ssafy.hp.user.response.UserExerciseResponse;
-import com.ssafy.hp.user.response.UserInfoResponse;
-import com.ssafy.hp.user.response.UserPillResponse;
-import com.ssafy.hp.user.response.UserReviewPillResponse;
+import com.ssafy.hp.user.response.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,12 +25,17 @@ import static com.ssafy.hp.NotFoundException.*;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
     private final AuthRepository authRepository;
     private final UserRepository userRepository;
     private final UserProfileRepository userProfileRepository;
     private final ExercisePurposeRepository exercisePurposeRepository;
     private final UserQueryRepository userQueryRepository;
+    private final ExerciseRepository exerciseRepository;
+    private final PillRepository pillRepository;
+    private final UserExerciseRepository userExerciseRepository;
+    private final UserPillRepository userPillRepository;
 
 
     // 회원정보 조회
@@ -68,6 +72,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public UserExerciseInfoResponse findByExerciseId(User user, int exerciseId) {
+        User findUser = userRepository.findById(user.getUserId())
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+        Exercise findExercise = exerciseRepository.findById(exerciseId)
+                .orElseThrow(() -> new NotFoundException(EXERCISE_NOT_FOUND));
+        UserExercise findUserExercise = userExerciseRepository.findUserExerciseByUsersAndExercise(findUser, findExercise)
+                .orElseThrow(() -> new NotFoundException(EXERCISE_NOT_FOUND));
+
+        return UserExerciseInfoResponse.from(findUserExercise);
+    }
+
+    @Override
     public Page<UserPillResponse> findTakingPillByUserId(User user, Pageable pageable) {
         User findUser = userRepository.findById(user.getUserId())
                 .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
@@ -89,6 +105,17 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
         Page<PillReview> findPillList = userQueryRepository.findReviewPillByUserId(user, pageable);
         return findPillList.map(UserReviewPillResponse::from);
+    }
+
+    @Override
+    public UserPillInfoResponse findByPillId(User user, int pillId) {
+        User findUser = userRepository.findById(user.getUserId())
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+        Pill findPill = pillRepository.findById(pillId)
+                .orElseThrow(() -> new NotFoundException(PILL_NOT_FOUND));
+        UserPill findUserPill = userPillRepository.findUserPillByUsersAndPill(findUser, findPill)
+                .orElseThrow(() -> new NotFoundException(PILL_NOT_FOUND));
+        return UserPillInfoResponse.from(findUserPill);
     }
 
     @Transactional
