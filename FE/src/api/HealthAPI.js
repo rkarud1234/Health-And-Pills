737d0e999a8ceaaf0db0cb946c25253ebdec5759
mindot7 <codemj11@gmail.com>
2022-09-20 11:50:32 +0900

    chore: 운동 api 설계

--- FE/src/api/HealthAPI.js
+++ FE/src/api/HealthAPI.js
@@ -1,44 +1,45 @@
 import { client } from "./index";
 
 // 운동 상세정보조회
-export const getExerciseDetail = async(exercise_id) => {
+export const getExerciseDetail = async(exerciseId) => {
   const result = await client
-    .get(`/exercise/${exercise_id}`)
-    .then((response) => response);
+    .get(`/exercise/${exerciseId}`)
+    .then((response) => response)
+    .catch((error) => error.response);
     return result;
 };
 
 // 현재 운동과 유사한 운동(스쿼트와 유사한 운동 추천)
-export const getExerciseItemReco = async(exercise_id) => {
+export const getExerciseItemReco = async(exerciseId) => {
   const result = await client
-    .get(`/exercise/recommend/item/${exercise_id}`)
+    .get(`/exercise/recommend/item/${exerciseId}`)
     .then((response) => response);
     return result;
-}
+};
 
 // 운동중 체크/취소
 export const exerciseDoing = async(data) => {
   const result = await client
-    .patch(`/exercise/doing`, data)
+    .post(`/exercise/doing`, data)
     .then((response) => response)
     .catch((error) => error);
   return result;
-}
+};
 
 // 좋아요/싫어요
 export const exerciseLike = async(data) => {
   const result = await client
-    .patch(`/exercise/like`, data)
+    .post(`/exercise/like`, data)
     .then((response) => response)
     .catch((error) => error);
   return result;
-}
+};
 
 // 북마크/취소
 export const exerciseBookMark = async(data) => {
   const result = await client
-    .patch(`/exercise/bookmark`, data)
+    .post(`/exercise/bookmark`, data)
     .then((response) => response)
     .catch((error) => error);
   return result;
-}
+};
