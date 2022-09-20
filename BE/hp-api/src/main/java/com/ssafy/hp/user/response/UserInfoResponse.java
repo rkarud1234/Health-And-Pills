783903c256c48de61a36ee8517d2fe3352107aa1
gksekqls9808 <gksekqls9808@naver.com>
2022-09-20 10:37:39 +0900

    fix: 회원정보조회 DTO 수정

--- BE/hp-api/src/main/java/com/ssafy/hp/user/response/UserInfoResponse.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/user/response/UserInfoResponse.java
@@ -5,8 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfoResponse {
     private String userProfileNickname;
 
