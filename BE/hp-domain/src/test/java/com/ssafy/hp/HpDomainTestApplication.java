6f512180cf256c93e7ba7be10fef4e40d91a03d3
gksekqls9808 <gksekqls9808@naver.com>
2022-09-21 01:08:58 +0900

    domain관련 테스트 코드 Application 생성

--- BE/hp-domain/src/test/java/com/ssafy/hp/HpDomainTestApplication.java
+++ BE/hp-domain/src/test/java/com/ssafy/hp/HpDomainTestApplication.java
@@ -1,2 +1,8 @@
-package com.ssafy.hp;public class HpDomainTestApplication {
+package com.ssafy.hp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpDomainTestApplication {
+
 }
