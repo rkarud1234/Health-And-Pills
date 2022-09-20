780e69815bce2a89905cd23a0964f6c8a6ddd1ea
이민우 <minwoo9629@naver.com>
2022-09-20 17:47:33 +0900

    Feat : 유저 프로필 생성 및 요청 로직

--- FE/src/api/users.js
+++ FE/src/api/users.js
@@ -1,13 +1,37 @@
-import { client } from "../utils/client";
+import { client } from "./index";
 
-const getProfile = async () => {
+export const getProfile = async () => {
   const result = await client.get("/users").then((response) => response);
   return result;
 };
 
-const createProfile = async (data) => {
+export const createProfile = async (data) => {
   const result = await client.post("/users", data).then((response) => response);
   return result;
 };
 
-export { getProfile };
+export const getUserInfo = async (type) => {
+  const result = await client
+    .get(`/users/${type}`)
+    .then((response) => response.json());
+  return result;
+};
+
+export const fetchUserExercise = async () => {
+  const result = await client
+    .get("/users/exercise")
+    .then((response) => response);
+  return result;
+};
+
+export const fetchUserPill = async () => {
+  const result = await client.get("/users/pill").then((response) => response);
+  return result;
+};
+
+export const fetchUserBookmark = async (type) => {
+  const result = await client
+    .get(`/users/${type}/bookmark`)
+    .then((response) => response);
+  return result;
+};
