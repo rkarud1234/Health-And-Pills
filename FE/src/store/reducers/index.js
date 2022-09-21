762349423a9fcdd7cb64fa6bed9383f9b016914c
Sang Jin <tkdwls8412@naver.com>
2022-09-21 13:59:55 +0900

    feat: 신체검사 페이지 결과 슬라이스 생성

--- FE/src/store/reducers/index.js
+++ FE/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import testSlice from "./testSlice"
 
 const reducer = combineReducers({
   user: userSlice.reducer,
+  sum: testSlice.reducer,
 });
 
 export default reducer;
