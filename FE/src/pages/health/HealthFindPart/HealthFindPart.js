6584cc620cbb56dd8189137dc23d0bdab88c8249
mindot7 <codemj11@gmail.com>
2022-09-26 01:26:05 +0900

    feat: 종류별, 부위별 운동 카드 불러오기

--- FE/src/pages/health/HealthFindPart/HealthFindPart.js
+++ FE/src/pages/health/HealthFindPart/HealthFindPart.js
@@ -1,11 +1,105 @@
+import HealthCard from "../../../components/cards/HealthCard";
+import { useEffect, useState, useCallback, useRef } from "react";
+import { client } from "../../../api";
+import styled from "styled-components";
+import { useInView } from "react-intersection-observer";
+import React from "react";
+
+const HealthButton = styled.button`
+background-color: ${({ color }) => color};
+color: ${({ textColor }) => textColor};
+font-size: 16px;
+cursor: pointer;
+`
+
 const HealthFindPart = () => {
+
+  const [part, setPart] = useState([]);
+  const [partItems, setPartItems] = useState([]);
+
+  const [partNum, setPartNum] = useState(1);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [ref, inView] = useInView();
+
+  // 운동 부위별 반환
+  const getExerPart = async () => {
+    await client
+      .get('/exercise/part-categories')
+      .then((res) => {
+        if (res.status === 200)
+        setPart([...res.data])
+      })
+      .catch((e) => e.res);
+  };
+  useEffect(() => {
+    getExerPart();
+  }, []);
+
+  // 운동 부위별 클릭시 렌더링
+  const onHandlePart = async (exercisePartCategoryId) => {
+    setPartNum(exercisePartCategoryId)
+  }
+
+  // 운동 부위별 조회
+  const getPartItems = useCallback(async (partNum) => {
+    setLoading(true);
+    await client
+      .get(`/exercise/part?part=${partNum}&page=${page}`)
+      .then((res) => {
+        setPartItems([...res.data.content]);
+      })
+      .catch((e) => {console.log(e)})
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setLoading(false);
+  }, [partNum, page]);
+  useEffect(() => {
+    getPartItems(1);
+  }, []);
+
+  // useEffect(() => {
+  //   // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
+  //   if (inView && !loading) {
+  //     setPage((prevState) => prevState + 1);
+  //   }
+  // }, [inView, loading]);
+
   return (
     <>
-      운동 부위별 찾기 페이지 <br />
-      - 부위별: 앱 참고해서 적당히(팔,다리같이 커다란 분류로 분리) <br/>
-      - 부위 : 태그 형식으로 여러 개 등록될 수 있음 <br/>
-      운동 		운동부위카테고리		   운동부위				        카테고리 <br/>
-      운동Id		운동부위카테고리Id          운동Id, 운동부위카테고리Id		카테고리Id
+        {part.map((parts) => (
+          <HealthButton
+          {...parts} key={parts.exercisePartCategoryId}
+          onClick={() => {onHandlePart(parts.exercisePartCategoryId); getPartItems(parts.exercisePartCategoryId)}}
+          >
+            {parts.exercisePartCategoryId}{parts.exercisePartCategoryName}
+          </HealthButton>
+        ))}
+        <div>
+          {partItems.map((item, idx) => (
+              <React.Fragment key={idx}>
+                {partItems.length - 1 == idx ? (
+                  <>
+                    <div ref={ref}>
+                      <HealthCard
+                        {...item}
+                        key={idx}
+                      >
+                      </HealthCard>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <HealthCard
+                      {...item}
+                      key={idx}
+                      width="320px"
+                    >
+                    </HealthCard>
+                  </>
+                )}
+              </React.Fragment>
+            ))}
+        </div>
     </>
   );
 };
--- FE/src/pages/health/HealthFindType.js
+++ FE/src/pages/health/HealthFindType.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import styled from "styled-components";
+import { client } from "../../api";
 import HealthFindBody from "../health/HealthFindPart/HealthFindBody";
 import HealthFindSports from "../health/HealthFindPart/HealthFindSports";
 import HealthFindGigu from "./HealthFindPart/HealthFindGigu";
+import HealthCard from "../../components/cards/HealthCard";
+import { useInView } from "react-intersection-observer";
+import React from "react";
 
 const TapWrapper = styled.div`
   width: 100vw;
@@ -15,41 +19,101 @@ font-size: 16px;
 cursor: pointer;
 `
 
-const HealthFindType = () => {
-  const [typePage, setTypePage] = useState("body")
-  const findPage = {
-    body: <HealthFindBody/>,
-    gigu: <HealthFindGigu/>,
-    sports: <HealthFindSports/>
+const HealthFindType = ({exerciseId}) => {
+  // const [typePage, setTypePage] = useState("body")
+  // const findPage = {
+  //   body: <HealthFindBody/>,
+  //   gigu: <HealthFindGigu/>,
+  //   sports: <HealthFindSports/>
+  // }
+
+  const [cate, setCate] = useState([]);
+  const [items, setItems] = useState([]);
+
+  const [cateNum, setCateNum] = useState(1);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [ref, inView] = useInView();
+  // 운동 종류별 반환
+  const getExerType = async () => {
+    await client
+      .get('/exercise/categories')
+      .then((res) => {
+        if (res.status === 200)
+        setCate([...res.data])
+      })
+      .catch((e) => e.res);
+  };
+  useEffect(() => {
+    getExerType();
+  }, []);
+
+  // 운동 종류별 클릭시 렌더링
+  const onHandleType = async (exerciseCategoryId) => {
+    setCateNum(exerciseCategoryId)
   }
 
+  // 운동 종류별 조회 --> cateNum, page 따로
+  const getCateItems = useCallback(async (cateNum) => {
+    setLoading(true);
+    await client
+      .get(`/exercise/category?category=${cateNum}&page=${page}`)
+      .then((res) => {
+        setItems([...res.data.content])
+        // setItems((prevState) => [...prevState, ...res.data.content])
+      })
+      .catch((e) => {console.log(e)})
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setLoading(false);
+  }, [cateNum, page]);
+  useEffect(() => {
+    getCateItems(1);
+  }, []);
+  // console.log(items)
+
+
+  // useEffect(() => {
+  //   // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
+  //   if (inView && !loading) {
+  //     setPage((prevState) => prevState + 1);
+  //   }
+  // }, [inView, loading]);
 
   return (
     <>
     <TapWrapper>
-      <HealthButton
-        onClick = {() => setTypePage("body")}
-        textColor = {typePage === "body" ? "#39C18E" : "#7b7b7b"}
-      >
-        맨몸
-      </HealthButton>
-      |
-      <HealthButton
-        onClick = {() => setTypePage("gigu")}
-        textColor = {typePage === "gigu" ? "#39C18E" : "#7b7b7b"}
-      >
-        기구
-      </HealthButton>
-      |
-      <HealthButton
-        onClick = {() => setTypePage("sports")}
-        textColor = {typePage === "sports" ? "#39C18E" : "#7b7b7b"}
-      >
-        스포츠
-      </HealthButton>
-      <div>
-        {findPage[typePage]}
-      </div>
+      {cate.map((cates) => (
+        <HealthButton
+          {...cates} key={cates.exerciseCategoryId}
+          onClick = {() => {onHandleType(cates.exerciseCategoryId); getCateItems(cates.exerciseCategoryId)}}
+        >
+          {cates.exerciseCategoryId}{cates.exerciseCategoryName}
+        </HealthButton>
+      ))}
+        {items.map((item, idx) => (
+            <React.Fragment key={idx}>
+              {items.length - 1 == idx ? (
+                <>
+                  <div ref={ref}>
+                    <HealthCard
+                      {...item}
+                      key={idx}
+                    >
+                    </HealthCard>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <HealthCard
+                    {...item}
+                    key={idx}
+                    width="320px"
+                  >
+                  </HealthCard>
+                </>
+              )}
+            </React.Fragment>
+          ))}
     </TapWrapper>
     </>
   )
