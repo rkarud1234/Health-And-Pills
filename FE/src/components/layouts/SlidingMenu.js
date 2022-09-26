6c3d23f01196d501dab26f2523346f924a16c5d8
이민우 <minwoo9629@naver.com>
2022-09-26 15:57:29 +0900

    Edit : 슬라이딩 메뉴 수정

--- FE/src/components/layouts/SlidingMenu.js
+++ FE/src/components/layouts/SlidingMenu.js
@@ -3,22 +3,23 @@ import BackButton from "../buttons/BackButton";
 import Header from "./Header";
 
 const SlidingMenuWrapper = styled.div`
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
+  max-width: 500px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
   top: 0px;
-  left: 0px;
+  /* left: 0px; */
   background-color: #fff;
   transition: transform 0.3s cubic-bezier(0, 0.52, 0, 1);
   &.hide {
-    transform: translate3d(100vw, 0, 0);
+    transform: translate3d(100%, 0, 0);
   }
   &.active {
     transform: translate3d(0, 0, 0);
   }
 `;
 
-const SlidingMenu = ({ type, active, close, slidingMenuTitle }) => {
+const SlidingMenu = ({ type, active, close, slidingMenuTitle, contents }) => {
   return (
     <>
       <SlidingMenuWrapper className={active ? "active" : "hide"}>
@@ -27,7 +28,7 @@ const SlidingMenu = ({ type, active, close, slidingMenuTitle }) => {
           leftNone={true}
           leftChildren={<BackButton onClick={close} />}
         />
-        {type}
+        {contents}
       </SlidingMenuWrapper>
     </>
   );
