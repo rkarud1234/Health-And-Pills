7024d2a944ed1902d0b5d07720ee082218ada716
gksekqls9808 <gksekqls9808@naver.com>
2022-09-27 10:52:13 +0900

    feat: 일정 API controller 단위 테스트코드 작성

--- BE/hp-api/src/test/java/com/ssafy/hp/calendar/CalendarFixture.java
+++ BE/hp-api/src/test/java/com/ssafy/hp/calendar/CalendarFixture.java
@@ -1,2 +1,29 @@
-package com.ssafy.hp.calendar;public class CalendarFixture {
+package com.ssafy.hp.calendar;
+
+import com.ssafy.hp.calendar.request.CreateCalendarRequest;
+import com.ssafy.hp.calendar.request.UpdateCalendarRequest;
+import com.ssafy.hp.calendar.response.CalendarCountListResponse;
+import com.ssafy.hp.calendar.response.CalendarDetailListResponse;
+
+import java.time.LocalTime;
+
+public class CalendarFixture {
+    public static final Integer ID = 1;
+    public static final Integer DATE = 1;
+    public static final Integer COUNT = 1;
+    public static final LocalTime TIME = LocalTime.now();
+    public static final String CONTENT = "내용";
+    public static final String NAME = "이름";
+    public static final String YN_FIELD = "Y";
+
+
+    public static final CreateCalendarRequest TEST_CALENDAR_CREATE_REQUEST
+            = new CreateCalendarRequest(ID, ID, CONTENT, DATE, TIME);
+    public static final UpdateCalendarRequest TEST_CALENDAR_UPDATE_REQUEST
+            = new UpdateCalendarRequest(CONTENT, TIME);
+
+    public static final CalendarCountListResponse TEST_CALENDAR_COUNT_LIST_RESPONSE
+            = new CalendarCountListResponse(DATE, COUNT, COUNT);
+    public static final CalendarDetailListResponse TEST_CALENDAR_DETAIL_LIST_RESPONSE
+            = new CalendarDetailListResponse(ID, ID, NAME, DATE, TIME, CONTENT, YN_FIELD);
 }
