9a6ac0a2f5ca054010a3fe6504163bfeab683304
gksekqls9808 <gksekqls9808@naver.com>
2022-09-27 10:52:48 +0900

    fix: 회원 API 테스트코드 수정

--- BE/hp-api/src/test/java/com/ssafy/hp/user/UserFixture.java
+++ BE/hp-api/src/test/java/com/ssafy/hp/user/UserFixture.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class UserFixture {
-    public static final User user = User.createUser(Provider.google, "109764055816357512970", "google_109764055816357512970", Role.ROLE_USER, "한싸피");
     public static final int ID = 1;
     public static final String NAME = "이름";
     public static final List<String> TYPE = Arrays.asList("부위1", "부위2", "부위3", "부위4");
@@ -25,13 +24,13 @@ public class UserFixture {
     public static final String USER_PROFILE_GENDER = "male";
     public static final int EXERCISE_ID = 1;
     public static final double USER_PROFILE_HEIGHT = 700;
-    public static final double USSER_PROFILE_WEIGHT = 70;
+    public static final double USER_PROFILE_WEIGHT = 70;
     public static final double USER_PROFILE_FAT = 77;
     public static final double USER_PROFILE_SKELETON = 77;
     public static final double USER_PROFILE_WATER = 77;
 
     public static final UserInfoResponse TEST_USER_INFO_RESPONSE
-            = new UserInfoResponse(USER_PROFILE_NICKNAME,USER_PROFILE_BIRTHDAY,USER_PROFILE_GENDER, EXERCISE_ID, EXERCISE_ID, USER_PROFILE_HEIGHT, USSER_PROFILE_WEIGHT, USER_PROFILE_FAT, USER_PROFILE_SKELETON, USER_PROFILE_WATER);
+            = new UserInfoResponse(USER_PROFILE_NICKNAME,USER_PROFILE_BIRTHDAY,USER_PROFILE_GENDER, EXERCISE_ID, EXERCISE_ID, USER_PROFILE_HEIGHT, USER_PROFILE_WEIGHT, USER_PROFILE_FAT, USER_PROFILE_SKELETON, USER_PROFILE_WATER);
     public static final UserExerciseResponse TEST_USER_EXERCISE_RESPONSE
             = new UserExerciseResponse(ID, ID, NAME, TYPE);
     public static final UserExerciseInfoResponse TEST_USER_EXERCISE_INFO_RESPONSE
@@ -48,5 +47,5 @@ public class UserFixture {
     public static final UpdateUserExerciseRequest TEST_UPDATE_USER_EXERCISE_REQUEST
             = new UpdateUserExerciseRequest(EXERCISE_ID, EXERCISE_ID);
     public static final UpdateUserInbodyRequest TEST_UPDATE_USER_INBODY_REQUEST
-            = new UpdateUserInbodyRequest(USER_PROFILE_HEIGHT, USSER_PROFILE_WEIGHT, USER_PROFILE_FAT, USER_PROFILE_SKELETON, USER_PROFILE_WATER);
+            = new UpdateUserInbodyRequest(USER_PROFILE_HEIGHT, USER_PROFILE_WEIGHT, USER_PROFILE_FAT, USER_PROFILE_SKELETON, USER_PROFILE_WATER);
 }
