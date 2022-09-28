c558deb8fcbb105d68a8be21a4fb28930a134ac1
gksekqls9808 <gksekqls9808@naver.com>
2022-09-28 17:34:31 +0900

    feat: 회원 정보의 인바디 평균값 API 작성

--- BE/hp-api/src/main/java/com/ssafy/hp/user/response/UserInbodyAverageResponse.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/user/response/UserInbodyAverageResponse.java
@@ -1,2 +1,31 @@
-package com.ssafy.hp.user.response;public class UserInbodyAverageResponse {
+package com.ssafy.hp.user.response;
+
+import com.ssafy.hp.user.domain.UserProfile;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInbodyAverageResponse {
+    private double userProfileHeight;
+
+    private double userProfileWeight;
+
+    private double userProfileFat;
+
+    private double userProfileSkeleton;
+
+    private double userProfileWater;
+
+    public static UserInbodyAverageResponse from(double userProfileHeight, double userProfileWeight, double userProfileFat, double userProfileSkeleton, double userProfileWater){
+        UserInbodyAverageResponse userInbodyAverageResponse = new UserInbodyAverageResponse();
+        userInbodyAverageResponse.userProfileHeight = userProfileHeight;
+        userInbodyAverageResponse.userProfileWeight = userProfileWeight;
+        userInbodyAverageResponse.userProfileFat = userProfileFat;
+        userInbodyAverageResponse.userProfileSkeleton = userProfileSkeleton;
+        userInbodyAverageResponse.userProfileWater = userProfileWater;
+        return userInbodyAverageResponse;
+    }
 }
