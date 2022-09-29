fd9d21f2fbaf08d084a91ecd269b3057258a6c9f
이민우 <minwoo9629@naver.com>
2022-09-30 01:14:35 +0900

    Feat : 리덕스 유저 액션 기능 추가

--- FE/src/store/actions/user.js
+++ FE/src/store/actions/user.js
@@ -1,10 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getProfile } from "../../api/users";
+import {
+  getProfile,
+  updateUserExercise,
+  updateUserinbody,
+} from "../../api/users";
 
 export const profile = createAsyncThunk(
   "user/profile",
   async (data, thunkAPI) => {
     const result = await getProfile();
     return result.data;
+  }
+);
+
+export const editLifestyle = createAsyncThunk(
+  "user/edit/lifestyle",
+  async (data, thunkAPI) => {
+    await updateUserExercise(data);
+  }
+);
+
+export const editInbody = createAsyncThunk(
+  "user/edit/inbody",
+  async (data, thunkAPI) => {
+    await updateUserinbody(data);
   }
 );
