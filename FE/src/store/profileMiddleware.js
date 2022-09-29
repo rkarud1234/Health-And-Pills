e755a2ffbd4dbd3661d78281db23a1b9bf5d8c23
이민우 <minwoo9629@naver.com>
2022-09-30 01:14:59 +0900

    Feat : 리덕스 미들웨어 로직 추가

--- FE/src/store/profileMiddleware.js
+++ FE/src/store/profileMiddleware.js
@@ -1,10 +1,19 @@
-import { profile } from "./actions/user";
+import { editInbody, editLifestyle, profile } from "./actions/user";
 import { logIn } from "./reducers/userSlice";
 
 export const profileMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case profile.pending.type:
-      store.dispatch(logIn());
+      const user = store.getState().user;
+      if (!user.isLogin) {
+        store.dispatch(logIn());
+      }
+      break;
+
+    case editLifestyle.fulfilled.type:
+    case editInbody.fulfilled.type:
+      store.dispatch(profile());
+      break;
     default:
       break;
   }
