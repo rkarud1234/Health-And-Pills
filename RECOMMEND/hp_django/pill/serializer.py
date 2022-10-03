d8234c692635c8c5aa2066a5993256f2331959a2
parksanghyeop <mon2210@naver.com>
2022-10-03 18:21:36 +0900

    fix: 영양제 리뷰 평균,갯수 오류 수정

--- RECOMMEND/hp_django/pill/serializer.py
+++ RECOMMEND/hp_django/pill/serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Pill
 
+class PillScoreSerializer(serializers.ModelSerializer):
+    pass
+
 class PillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pill        # Pill 모델 사용
-        fields = ['pill_id','pill_name','pill_company_name','review_average','review_count','pill_thumbnail'] 
+        fields = ['pill_id','pill_name','pill_company_name','pill_thumbnail'] 
--- RECOMMEND/hp_django/pill/urls.py
+++ RECOMMEND/hp_django/pill/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from pill.views import RecommendCustomApi, RecommendBestApi, RecommendItemApi, RecommendUserApi
 
 urlpatterns = [
-    path('custom/<int:userId>', RecommendCustomApi.as_view()),
-    path('best/<int:userId>', RecommendBestApi.as_view()),
+    path('custom/<int:userId>/', RecommendCustomApi.as_view()),
+    path('best/<int:userId>/', RecommendBestApi.as_view()),
     path('user/<int:userId>/', RecommendUserApi.as_view()),
     path('item/<int:userId>/<int:pillId>/', RecommendItemApi.as_view()),
 ]
