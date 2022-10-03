1d601bb3b8ebc1a8581b3d64bbe2e0dc000d7765
이민우 <minwoo9629@naver.com>
2022-10-03 15:12:23 +0900

    Edit : useFetchData 훅 수정

--- FE/src/hooks/useFetchData.js
+++ FE/src/hooks/useFetchData.js
@@ -13,12 +13,14 @@ const useFetchData = (fetchUrl, queryKey, updateFn, deleteFn) => {
   const updateMutation = useMutation(updateFn, {
     onSuccess: () => {
       queryClient.invalidateQueries(queryKey);
+      alert("수정되었습니다.");
     },
   });
 
   const deleteMutation = useMutation(deleteFn, {
     onSuccess: () => {
       queryClient.invalidateQueries([queryKey]);
+      alert("삭제되었습니다.");
     },
   });
   return { res, updateMutation, deleteMutation };
