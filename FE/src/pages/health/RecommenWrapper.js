3acc456b16f7a7ad8e3210413a9c3d9fe794da84
Sang Jin <tkdwls8412@naver.com>
2022-10-05 11:39:03 +0900

    feat: 로딩 스피너 적용

--- FE/src/pages/health/RecommenWrapper.js
+++ FE/src/pages/health/RecommenWrapper.js
@@ -1,11 +1,7 @@
 import HealthCard from "../../components/cards/HealthCard";
 import styled from "styled-components";
-import Carousel from "../../components/carousel/Carousel";
-import { useRef, useState, useEffect } from 'react'
-import { getExerciseBest, getExerciseCustom, getExerciseUser } from "../../api/HealthAPI";
+import { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-
-    
 const RecoDivWrapper = styled.div`
     padding: 4px;
     `
@@ -25,26 +21,16 @@ const FlexBox = styled.div`
     padding: 16px 0px 24px;
     `
 
-const RecommendWrapper = ({type}) => {
-    const navigate = useNavigate()
-    
-    const scrollRef = useRef(null);
-    const [isDrag, setIsDrag] = useState(false);
-    const [startX, setStartX] = useState();
-    const [startPageX, setStartPageX] = useState();
-    const [endPageX, setendPageX] = useState();
-    
-    const category = {
-        best: '베스트 운동',
-        custom: '사용자 맞춤 운동',
-        user: '00대 00 운동'
-    }
-    const requestType = {
-        best: getExerciseBest(),
-        custom: getExerciseCustom(),
-        user: getExerciseUser()
-    }
-    // console.log(category[type])
+const RecommendWrapper = ({
+  text,
+  exercises }) => {
+  const navigate = useNavigate()
+
+  const scrollRef = useRef(null);
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState();
+  const [startPageX, setStartPageX] = useState();
+  const [endPageX, setendPageX] = useState();
 
   const onDragStart = (e) => {
     e.preventDefault();
@@ -63,50 +49,38 @@ const RecommendWrapper = ({type}) => {
       scrollRef.current.scrollLeft = startX - e.pageX;
     }
   };
-    // 베스트 운동 추천
-    const [exercises, setExercises] = useState([]);
-    const getExercise = async () => {
-        
-        const response = await getExerciseBest();
-        if (response.status === 200)
-        setExercises([...response.data])
-        console.log(response.data)
-    };
-    useEffect(() => {
-    getExercise();
-  }, [exercises.id])
-
-    return (
-        <RecoDivWrapper>
-            <RecomWrapper>
-                {category[type]}
-            </RecomWrapper>
-            <FlexBox
-            ref={scrollRef}
-            onMouseDown={onDragStart}
-            onMouseMove={onDragMove}
-            onMouseUp={onDragEnd}
-            onMouseLeave={onDragEnd}
-        >
-            {exercises.map((exercise) => (
-                <div className="onclick-div"
-                onClick={(e) => {
-                    if (startPageX === endPageX) {
-                    navigate(`/health/detail/${exercise.id}`)
-                    }
-                }}
-                >
-                <HealthCard
-                {...exercise} key={exercise.id}
-                exerciseName={exercise.name}
-                exerciseId={exercise.id}
-                exerciseParts={exercise.parts} />
-                </div>
-            )                
-            )}
-        </FlexBox>
-      </RecoDivWrapper>
-    )
+  return (
+    <RecoDivWrapper>
+      <RecomWrapper>
+        {text}
+      </RecomWrapper>
+      <FlexBox
+        ref={scrollRef}
+        onMouseDown={onDragStart}
+        onMouseMove={onDragMove}
+        onMouseUp={onDragEnd}
+        onMouseLeave={onDragEnd}
+      >
+        {exercises && exercises.map((exercise) => (
+          <div className="onclick-div"
+            key={exercise.id}
+            onClick={(e) => {
+              if (startPageX === endPageX) {
+                navigate(`/health/detail/${exercise.id}`)
+              }
+            }}
+          >
+            <HealthCard
+              {...exercise} key={exercise.id}
+              exerciseName={exercise.name}
+              exerciseId={exercise.id}
+              exerciseParts={exercise.parts} />
+          </div>
+        )
+        )}
+      </FlexBox>
+    </RecoDivWrapper>
+  )
 }
 
 export default RecommendWrapper
