5ebc51d6f0524b4bf6a5b3a28071f951129d148d
parksanghyeop <mon2210@naver.com>
2022-10-06 05:29:46 +0900

    style: 일정관리 페이지 일정 리스트 css 적용

--- FE/src/components/cards/DailyDetailCard.js
+++ FE/src/components/cards/DailyDetailCard.js
@@ -9,26 +9,35 @@ import { client } from "../../api";
 
 const DailyListWrapper = styled.div`
   display: block;
-`
+`;
 const DailyItemWrapper = styled.div`
   display: flex;
-`
+  width: 100%;
+  padding-left: 12px;
+`;
 
 const HpIconWrapper = styled.div`
-  font-size: small;
-  background: linear-gradient(#537CFE, #6A53FE);
+  background: linear-gradient(#537cfe, #6a53fe);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-`
+`;
 
 const ScheduleListWrapper = styled.button`
   background-color: transparent;
-`
+`;
 
 const DailyDetailCard = ({
-  calendarComplete, calendarContent, calendarDate,
-  calendarId, calendarTime, exerciseId, name,
-  pillId, onToggleScheduleDone, flag, setFlag
+  calendarComplete,
+  calendarContent,
+  calendarDate,
+  calendarId,
+  calendarTime,
+  exerciseId,
+  name,
+  pillId,
+  onToggleScheduleDone,
+  flag,
+  setFlag,
 }) => {
   // 시, 분 까지만 자르기
   const scheduleTime = calendarTime.toString().substr(0, 5);
@@ -43,7 +52,17 @@ const DailyDetailCard = ({
   const closeModal = () => {
     setIsOpen(false);
   };
+  console.log("캔린더컴플리트", calendarComplete);
 
+  let cardColor = "#fff";
+  let cardFontColor = "#000";
+  let cardIconColor = "#537cfe";
+
+  if (calendarComplete === "Y") {
+    cardColor = "#e0e0e0";
+    cardFontColor = "#999999";
+    cardIconColor = "#999999";
+  }
 
   return (
     <>
@@ -61,34 +80,77 @@ const DailyDetailCard = ({
             closeModal={closeModal}
             flag={flag}
             setFlag={setFlag}
-          />}
+          />
+        }
         closeButton={<ModalCloseButton onClick={closeModal} />}
       />
-      <DailyListWrapper>
-        <DailyItemWrapper>
-          {scheduleTime} |
-          <HpIconWrapper>
-            {exerciseId === null ? <i className="fa-regular fa-capsules" /> : <i className="fa-solid fa-dumbbell" />}
-          </HpIconWrapper>
+
+      <DailyItemWrapper
+        style={{
+          width: "90%",
+          margin: "12px auto",
+          borderRadius: "8px",
+          backgroundColor: cardColor,
+          color: cardFontColor,
+          boxShadow:
+            "0 1px 3px 0 rgba(0, 0, 0, 0.1),0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+        }}
+      >
+        <div
+          style={{
+            margin: "10px 0",
+            padding: "4px 6px",
+            borderRadius: "6px",
+            color: "#565656",
+            // boxShadow:
+            //   "0 1px 3px 0 rgba(0, 0, 0, 0.1),0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+          }}
+        >
+          {scheduleTime}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            margin: "0 24px 0 12px",
+            width: "100%",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              color: cardIconColor,
+            }}
+          >
+            {exerciseId === null ? (
+              <i className="fa-regular fa-capsules" />
+            ) : (
+              <i className="fa-solid fa-dumbbell" />
+            )}
+          </div>
           <ScheduleListWrapper
+            style={{
+              color: cardFontColor,
+            }}
             name={name}
             onClick={() => {
               openModal();
-            }}>
-            {name} |
+            }}
+          >
+            {name}
           </ScheduleListWrapper>
-          <HpIconWrapper>
-            <ScheduleDone
-              calendarComplete={calendarComplete}
-              onClick={() => {
-                onToggleScheduleDone(calendarId)
-              }}
-            />
-          </HpIconWrapper>
-        </DailyItemWrapper>
-      </DailyListWrapper>
+        </div>
+
+        <HpIconWrapper style={{ marginRight: "12px", paddingTop: "11px" }}>
+          <ScheduleDone
+            calendarComplete={calendarComplete}
+            onClick={() => {
+              onToggleScheduleDone(calendarId);
+            }}
+          />
+        </HpIconWrapper>
+      </DailyItemWrapper>
     </>
-  )
+  );
 };
 
 export default DailyDetailCard;
