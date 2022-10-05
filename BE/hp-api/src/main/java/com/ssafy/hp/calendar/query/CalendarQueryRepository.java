ba1f93e24c5bb557531c939a31c350f0443d7319
gksekqls9808 <gksekqls9808@naver.com>
2022-10-06 02:13:27 +0900

    feat: FCM 알림 기능 구현

--- BE/hp-api/src/main/java/com/ssafy/hp/calendar/query/CalendarQueryRepository.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/calendar/query/CalendarQueryRepository.java
@@ -6,11 +6,16 @@ import com.ssafy.hp.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import static com.ssafy.hp.calendar.domain.QCalendar.calendar;
+import static com.ssafy.hp.common.type.YN.N;
+import static com.ssafy.hp.common.type.YN.Y;
 import static com.ssafy.hp.exercise.domain.QExercise.exercise;
 import static com.ssafy.hp.pill.domain.QPill.pill;
+import static com.ssafy.hp.user.domain.QUser.user;
 
 @Repository
 @RequiredArgsConstructor
@@ -25,5 +30,43 @@ public class CalendarQueryRepository {
                 .where(calendar.calendarDate.eq(calendarDate),
                         calendar.users.eq(user))
                 .fetch();
+    }
+
+    public List<Calendar> findByCalendarTimeAndNotificationSend(){
+        //5분 이상 차이나면서 처리가 안된것 조회 (queryDsl)
+
+        return queryFactory
+                .selectFrom(calendar)
+                .leftJoin(calendar.users, user).fetchJoin()
+                .where(calendar.calendarTime.lt(LocalTime.from(LocalDateTime.now().plusMinutes(5))),
+                        calendar.notificationSend.eq(N))
+                .fetch();
+    }
+
+    public void updateNotificationSend(List<Calendar> findCalendars){
+        queryFactory
+                .update(calendar)
+                .set(calendar.notificationSend, Y)
+                .where(calendar.in(findCalendars))
+                .execute();
+
+    }
+
+    public List<Calendar> findByCalendarTimeAndCalendarCompleteAndCompleteSend(){
+        return queryFactory
+                .selectFrom(calendar)
+                .leftJoin(calendar.users, user).fetchJoin()
+                .where(calendar.calendarTime.gt(LocalTime.from(LocalDateTime.now().minusHours(1))),
+                        calendar.completeSend.eq(N),
+                        calendar.calendarComplete.eq(N))
+                .fetch();
+    }
+
+    public void updateCompleteSend(List<Calendar> findCalendars){
+        queryFactory
+                .update(calendar)
+                .set(calendar.completeSend, Y)
+                .where(calendar.in(findCalendars))
+                .execute();
     }
 }
--- BE/hp-api/src/main/java/com/ssafy/hp/calendar/service/CalendarServiceImpl.java
+++ BE/hp-api/src/main/java/com/ssafy/hp/calendar/service/CalendarServiceImpl.java
@@ -14,6 +14,7 @@ import com.ssafy.hp.common.type.YN;
 import com.ssafy.hp.exercise.ExerciseRepository;
 import com.ssafy.hp.exercise.domain.Exercise;
 import com.ssafy.hp.exercise.service.ExerciseService;
+import com.ssafy.hp.fcm.FirebaseClient;
 import com.ssafy.hp.pill.PillRepository;
 import com.ssafy.hp.pill.domain.Pill;
 import com.ssafy.hp.pill.service.PillService;
@@ -48,6 +49,7 @@ public class CalendarServiceImpl implements CalendarService{
     private final UserExerciseRepository userExerciseRepository;
     private final ExerciseService exerciseService;
     private final PillService pillService;
+    private final FirebaseClient firebaseClient;
 
     @Override
     // 회원의 요일별 영양제 & 운동 갯수 조회
@@ -139,9 +141,36 @@ public class CalendarServiceImpl implements CalendarService{
         }
     }
 
-    @Scheduled(cron = "0 0 * * 1")
+    //매주 일요일 0시 0분 정각에 수행
+    @Scheduled(cron = "0 0 0 * * 0")
     @Transactional
     public void deleteAll() {
         calendarRepository.deleteAll();
+    }
+
+    // 5분 전에 일정 수행하라고 알림
+    @Scheduled(cron = "* * * * * *")
+    @Transactional
+    public void sendNotificationCalendar(){
+        // 5분 이상 차이나면서 처리가 안된것 조회 (queryDsl)-> update 처리 (queryDsl 벌크성 쿼리)
+        List<Calendar> findCalendars = calendarQueryRepository.findByCalendarTimeAndNotificationSend();
+        calendarQueryRepository.updateNotificationSend(findCalendars);
+        // 해당 애들의 fcm토큰으로 알림 보내기
+        findCalendars
+                .forEach(calendar -> firebaseClient.send(calendar.getUsers().getFcmToken(), calendar.getUsers().getUserNickname() + "님! 일정을 수행해주세요!"));
+    }
+
+
+    // 일정시간 후에 1시간 지나도 미완료상태일때 -> 일정 수행하라고 알림
+    @Scheduled(cron = "* * * * * *")
+    @Transactional
+    public void sendNotificationNotCompleteCalendar(){
+        // 1시간 이상 애들 중에 미완료 상태이고 알림이 안보내진것 조회 -> Update 처리
+        List<Calendar> findCalendars = calendarQueryRepository.findByCalendarTimeAndCalendarCompleteAndCompleteSend();
+        calendarQueryRepository.updateCompleteSend(findCalendars);
+        // 해당 애들의 fcm토큰으로 알림 보내기
+        findCalendars
+                .forEach(calendar -> firebaseClient.send(calendar.getUsers().getFcmToken(), calendar.getUsers().getUserNickname() + "님! 아직 완료되지 않은 일정이 있습니다!"));
+
     }
 }
