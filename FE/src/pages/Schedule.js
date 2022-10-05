7332e1f77e398e7d5ebdd838f6c764f232b5f0fc
Sang Jin <tkdwls8412@naver.com>
2022-10-05 16:05:21 +0900

    design: 데일리 카드 디자인 수정

--- FE/src/pages/Schedule.js
+++ FE/src/pages/Schedule.js
@@ -21,7 +21,8 @@ const BackWrapper = styled.div`
 
 const WeeklyWrapper = styled.div`
   display: flex;
-  height: 100px;
+  width: 100%;
+  margin-bottom: 32px;
   justify-content: space-around;
   /* background-color: transparent; */
 `
@@ -40,7 +41,7 @@ const Schedule = () => {
   // 모달 설정
   const [isOpen, setIsOpen] = useState(false);
   const [schedulePage, setSchedulePage] = useState("");
-  const modalPage = {scheduleCreate: <ScheduleCreate/>, pillScheduleCreate: <PillScheduleCreate/>};
+  const modalPage = { scheduleCreate: <ScheduleCreate />, pillScheduleCreate: <PillScheduleCreate /> };
 
   const openModal = () => {
     setIsOpen(true);
@@ -60,64 +61,64 @@ const Schedule = () => {
   const yearTwo = date.year
   const yearLastTwo = yearTwo.toString().slice(-2);
   const weekly = ['일', '월', '화', '수', '목', '금', '토'];
-  const monthFirstDay = new Date(date.year, (date.month) -1 , 1).getDay()
-  const nthWeek = ((date.date + monthFirstDay -1) % 7) -1
+  const monthFirstDay = new Date(date.year, (date.month) - 1, 1).getDay()
+  const nthWeek = ((date.date + monthFirstDay - 1) % 7) - 1
 
- // 오늘의 요일 설정
-const [yoil, setYoil] = useState(date.day);
+  // 오늘의 요일 설정
+  const [yoil, setYoil] = useState(date.day);
 
-// 다른 요일로 바꾸기
-const onHandleYoil = async (calendarDate) => {
-  setYoil(calendarDate)
-};
+  // 다른 요일로 바꾸기
+  const onHandleYoil = async (calendarDate) => {
+    setYoil(calendarDate)
+  };
 
-// 각 요일에 운, 영 개수 집어넣기
-const [list, setList] = useState([]);
+  // 각 요일에 운, 영 개수 집어넣기
+  const [list, setList] = useState([]);
 
-// 일정이 존재하지 않을 때 카드 개수 모자라는거 처리
-const getInfo = async () => {
-  const response = await getYoilInfo();
-  const array = new Array(7);
-  for(let data of response.data){
-    array[data.calendarDate]= data
-  }
-  for(let i=0;i<7;i++){
-    if(!array[i]){
-      array[i] ={calendarDate:i, pillCount:0, exerciseCount:0}
+  // 일정이 존재하지 않을 때 카드 개수 모자라는거 처리
+  const getInfo = async () => {
+    const response = await getYoilInfo();
+    const array = new Array(7);
+    for (let data of response.data) {
+      array[data.calendarDate] = data
     }
+    for (let i = 0; i < 7; i++) {
+      if (!array[i]) {
+        array[i] = { calendarDate: i, pillCount: 0, exerciseCount: 0 }
+      }
+    }
+    setList([...array])
+  };
+
+  // 요일별 상세 일정 불러오기 (디폴트는 오늘 요일)
+  const getDetail = async () => {
+    const response = await getYoilDetail(yoil);
+    setDetail([...response.data])
   }
-  setList([...array])
-};
 
-// 요일별 상세 일정 불러오기 (디폴트는 오늘 요일)
-const getDetail = async () => {
-  const response = await getYoilDetail(yoil);
-  setDetail([...response.data])
-}
+  // 요일별 상세 일정 변수 초기화
+  const [detail, setDetail] = useState([]);
+  // console.log(detail)
 
-// 요일별 상세 일정 변수 초기화
-const [detail, setDetail] = useState([]);
-// console.log(detail)
-
-// 일정 완료 체크할때 리렌더링용
-const [flag, setFlag] = useState(false);
-useEffect(() => {
-  getInfo();
-}, []);
-useEffect(() => {
-  getDetail();
- }, [yoil, flag]);
+  // 일정 완료 체크할때 리렌더링용
+  const [flag, setFlag] = useState(false);
+  useEffect(() => {
+    getInfo();
+  }, []);
+  useEffect(() => {
+    getDetail();
+  }, [yoil, flag]);
 
 
-// 일정 완료 체크 or 해제
-const onToggleScheduleDone = async (calendarId) => {
-  const response = await doneSchedule(calendarId);
-  if (response.status === 200) {
-    setFlag((prevState) => {
-      return {...prevState, flag: !flag}
-    });
-  }
-};
+  // 일정 완료 체크 or 해제
+  const onToggleScheduleDone = async (calendarId) => {
+    const response = await doneSchedule(calendarId);
+    if (response.status === 200) {
+      setFlag((prevState) => {
+        return { ...prevState, flag: !flag }
+      });
+    }
+  };
 
 
   return (
@@ -130,37 +131,37 @@ const onToggleScheduleDone = async (calendarId) => {
             yoil={yoil}
           />
         }
-        // modalContent={modalPage[schedulePage]}
-        // closeButton={<ModalCloseButton onClick={closeModal} />}
-        // yoil={yoil}
+      // modalContent={modalPage[schedulePage]}
+      // closeButton={<ModalCloseButton onClick={closeModal} />}
+      // yoil={yoil}
       />
-      <Header leftNone={true} leftChildren={<BackButton />}/>
-        <BackWrapper>
-          <div style={{textAlign: "center", padding: "12px 0 24px 0", fontWeight: "600"}}>
-            {yearLastTwo}년 {date.month}월 {nthWeek}주차
+      <Header leftNone={true} leftChildren={<BackButton />} />
+      <BackWrapper>
+        <div style={{ textAlign: "center", padding: "12px 0 24px 0", fontWeight: "600" }}>
+          {yearLastTwo}년 {date.month}월 {nthWeek}주차
+        </div>
+        <WeeklyWrapper>
+          <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+            {list.map((item, idx) => (
+              <DailyCard
+                {...item}
+                key={idx}
+                {...weekly}
+                onHandleYoil={onHandleYoil}
+                yoil={yoil}
+              />
+            ))}
           </div>
-          <WeeklyWrapper>
-            <div style={{display: "flex"}}>
-              {list.map((item, idx) => (
-                <DailyCard
-                  {...item}
-                  key={idx}
-                  {...weekly}
-                  onHandleYoil={onHandleYoil}
-                  yoil={yoil}
-                />
-              ))}
-            </div>
-          </WeeklyWrapper>
-          <ButtonWrapper
-            onClick={() => {
-              openModal();
-              setSchedulePage("scheduleCreate");
-            }}
-          >
-            <SchedulePlusButton/>
-          </ButtonWrapper>
-          {/* <ButtonWrapper
+        </WeeklyWrapper>
+        <ButtonWrapper
+          onClick={() => {
+            openModal();
+            setSchedulePage("scheduleCreate");
+          }}
+        >
+          <SchedulePlusButton />
+        </ButtonWrapper>
+        {/* <ButtonWrapper
             onClick={() => {
               openModal();
               setSchedulePage("pillScheduleCreate");
@@ -168,21 +169,21 @@ const onToggleScheduleDone = async (calendarId) => {
           >
             <SchedulePlusButton/>
           </ButtonWrapper> */}
-          <div style={{textAlign: "center"}}>
-            {detail.length !== 0 ? (
-              <div>
-                {detail.map((item, idx) => (
-                  <DailyDetailCard {...item} key={idx} onToggleScheduleDone={onToggleScheduleDone}/>
-                ))}
-              </div>
-            ) : (
-              <>
-                등록된 일정이 없습니다
-              </>
-            )}
-          </div>
-        </BackWrapper>
-      <Footer/>
+        <div style={{ textAlign: "center" }}>
+          {detail.length !== 0 ? (
+            <div>
+              {detail.map((item, idx) => (
+                <DailyDetailCard {...item} key={idx} onToggleScheduleDone={onToggleScheduleDone} />
+              ))}
+            </div>
+          ) : (
+            <>
+              등록된 일정이 없습니다
+            </>
+          )}
+        </div>
+      </BackWrapper>
+      <Footer />
     </>
   )
 }
