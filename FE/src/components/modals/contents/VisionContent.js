14c1d173f0ed440487e71bb4c2d087cd676547bd
parksanghyeop <mon2210@naver.com>
2022-10-06 10:47:44 +0900

    fix: 이미지검색 사진첨부 테두리 추가

--- FE/src/components/modals/contents/VisionContent.js
+++ FE/src/components/modals/contents/VisionContent.js
@@ -1,26 +1,26 @@
-import { useState, useRef } from 'react';
-import Cropper from 'react-cropper';
-import 'cropperjs/dist/cropper.css';
-import { SearchImg } from '../../../store/actions/search';
-import styled from 'styled-components';
+import { useState, useRef } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import { SearchImg } from "../../../store/actions/search";
+import styled from "styled-components";
 
 const FlexDiv = styled.div`
-display: flex;
-justify-content: space-between;
-width: 100%;
-margin-top: 16px;
-`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 16px;
+`;
 const TextDiv = styled.div`
-text-align: center;
-margin: 0px 8px;
-width: 100%;
-background: linear-gradient(180deg, #537CFE 0%, #6A53FE 100%);
--webkit-background-clip: text;
--webkit-text-fill-color: transparent;
-background-clip: text;
-text-fill-color: transparent;
-padding: 2px 0px;
-`
+  text-align: center;
+  margin: 0px 8px;
+  width: 100%;
+  background: linear-gradient(180deg, #537cfe 0%, #6a53fe 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+  padding: 2px 0px;
+`;
 const CustomBtn = styled.button`
   font-size: 16px;
   cursor: pointer;
@@ -43,9 +43,8 @@ const CustomBtn = styled.button`
     border-color: black;
     color:black;
   }
-  `
+  `;
 const VisionContent = ({ setSearchWord, close }) => {
-
   const cropperRef = useRef(null);
   // 유저가 첨부한 이미지
   const [inputImage, setInputImage] = useState(null);
@@ -57,39 +56,67 @@ const VisionContent = ({ setSearchWord, close }) => {
     setCroppedImage(cropper.getCroppedCanvas().toDataURL());
   };
 
-  let convertImage = ''
+  let convertImage = "";
   if (croppedImage) {
-    convertImage = croppedImage.replace(/^data:image\/(png|jpg);base64,/, "")
+    convertImage = croppedImage.replace(/^data:image\/(png|jpg);base64,/, "");
   }
 
   const requestHandler = () => {
-    SearchImg(convertImage)
-      .then(res => {
-        setSearchWord(res);
-      })
+    SearchImg(convertImage).then((res) => {
+      setSearchWord(res);
+    });
     close();
-  }
+  };
 
   return (
     <div>
-      <TextDiv style={{ fontSize: "24px", color: 'black' }}>
+      <TextDiv style={{ fontSize: "24px", color: "black" }}>
         사진으로 검색
       </TextDiv>
-      <TextDiv style={{ fontSize: "12px", marginBottom: '6px' }}>
+      <TextDiv style={{ fontSize: "12px", marginBottom: "6px" }}>
         주변에 있는 영양제를 찍어 검색해보세요!
       </TextDiv>
-      <div >
-        <input hidden type="file" id="file" accept="image/*" onChange={(e) => setInputImage(URL.createObjectURL(e.target.files[0]))} />
+      <div>
+        <input
+          hidden
+          type="file"
+          id="file"
+          accept="image/*"
+          onChange={(e) =>
+            setInputImage(URL.createObjectURL(e.target.files[0]))
+          }
+        />
         <label htmlFor="file">
-          <div style={{ display: "flex", padding: "16px 0px" }}>
+          <div
+            style={{
+              display: "flex",
+              padding: "16px 0px",
+              border: "1px solid #c1c1c1",
+              borderRadius: "8px",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#777777",
+              cursor: "pointer",
+            }}
+          >
             <i className="fa-solid fa-image fa-lg"></i>
-            <div style={{ lineHeight: '6px', paddingLeft: '4px' }}>사진 첨부</div>
+            <div
+              style={{
+                padding: "7px 4px 4px 4px",
+              }}
+            >
+              사진 첨부
+            </div>
           </div>
         </label>
-
-      </div >
+      </div>
       <Cropper
-        style={{ height: "100%", width: "100%", maxWidth: "360px", maxHeight: "360px" }}
+        style={{
+          height: "100%",
+          width: "100%",
+          maxWidth: "360px",
+          maxHeight: "360px",
+        }}
         src={inputImage}
         crop={onCrop}
         ref={cropperRef}
@@ -105,15 +132,18 @@ const VisionContent = ({ setSearchWord, close }) => {
         <div>
           <img width="200px" height="100px" src={croppedImage} />
         </div>
-        <FlexDiv style={{ flexDirection: 'column', justifyContent: 'flex-end' }}>
+        <FlexDiv
+          style={{ flexDirection: "column", justifyContent: "flex-end" }}
+        >
           <TextDiv>필요한 부분을 자른 후</TextDiv>
           <TextDiv>검색 버튼 클릭</TextDiv>
-          <CustomBtn onClick={requestHandler} className='search'>검색</CustomBtn>
+          <CustomBtn onClick={requestHandler} className="search">
+            검색
+          </CustomBtn>
         </FlexDiv>
       </FlexDiv>
-    </div >
-
+    </div>
   );
-}
+};
 
 export default VisionContent;
